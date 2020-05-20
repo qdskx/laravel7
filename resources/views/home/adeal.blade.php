6c2dc241da73f72fee0655f8c3017cadd2ecd242
qdskx <qdskx@outlook.com>
2020-05-20 16:12:28 +0800

    '呦呦呦'

--- resources/views/home/adeal.blade.php
+++ resources/views/home/adeal.blade.php
@@ -7,7 +7,7 @@
 <form action="testQuery" method="post" >
     <input type= "hidden"  name="token" value="{{csrf_token()}}">
     <input type="text" name="user" />
-    <input type="submit" value="提交">
+    <input type="submit" value="提交dddddddd">
 </form>
 </body>
 </html>
